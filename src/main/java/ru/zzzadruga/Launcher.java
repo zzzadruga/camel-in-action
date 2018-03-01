4a3030a4ea2547b964a6ad747ccd118df46f8b28
zzzadruga <zzzadruga@gmail.com>
2018-03-01 16:58:37 +0300

    Edited launcher for Spring launch

--- src/main/java/ru/zzzadruga/Launcher.java
+++ src/main/java/ru/zzzadruga/Launcher.java
@@ -2,20 +2,15 @@ package ru.zzzadruga;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.camel.CamelContext;
+import org.apache.camel.Main;
 import org.apache.camel.component.jms.JmsComponent;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Launcher {
     public static void main(String[] args) throws Exception {
-        CamelContext camelContext = new DefaultCamelContext();
-        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
-        connectionFactory.setBrokerURL("tcp://localhost:61616");
-        connectionFactory.setUserName("admin");
-        connectionFactory.setPassword("admin");
-        camelContext.addComponent("activemq", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
-        camelContext.addRoutes(new FtpToJmsCamelRoute());
-        camelContext.start();
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
         Thread.sleep(10000);
-        camelContext.stop();
     }
 }
