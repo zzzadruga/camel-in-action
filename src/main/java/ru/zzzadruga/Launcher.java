57dec7b4b80ea903e694fb00721f59bdb9a5b235
zzzadruga <zzzadruga@gmail.com>
2018-03-01 15:37:10 +0300

    Created connection for AMQ and add new route

--- src/main/java/ru/zzzadruga/Launcher.java
+++ src/main/java/ru/zzzadruga/Launcher.java
@@ -1,14 +1,21 @@
 package ru.zzzadruga;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.camel.CamelContext;
+import org.apache.camel.component.jms.JmsComponent;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class Launcher {
     public static void main(String[] args) throws Exception {
         CamelContext camelContext = new DefaultCamelContext();
-        camelContext.addRoutes(new FileCopierCamelRoute());
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+        connectionFactory.setBrokerURL("tcp://localhost:61616");
+        connectionFactory.setUserName("admin");
+        connectionFactory.setPassword("admin");
+        camelContext.addComponent("activemq", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+        camelContext.addRoutes(new FtpToJmsCamelRoute());
         camelContext.start();
-        Thread.sleep(1000);
+        Thread.sleep(10000);
         camelContext.stop();
     }
 }
